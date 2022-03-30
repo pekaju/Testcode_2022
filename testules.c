4a8a9950c874be990a49f23ceab55c91eb843ece
Peeter Kaju <102664489+pekaju@users.noreply.github.com>
2022-03-30 16:56:55 +0300

    Update testules.c

--- testules.c
+++ testules.c
@@ -6,24 +6,8 @@
  * which will be 3 days before each payment day as a reminder.
  * @date 2022-03-29
  */
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
 
-// number of months in a year is 12 and 10 is the date when employers are paid.
-#define MONTHS 12
-#define DAY 10
-
-// define sunday as number 7 and saturday as number 6
-#define SUNDAY 7
-#define SATURDAY 6
-
-int dayOfPayment ( int, int );
-int findWeekDay ( int, int, int );
-int aprilDate ( int, int );
-int otherDate ( int );
-int findBigFriday ( int );
-void fileWriter ( int[], int[], int );
+#include "header.h"
 
 int main ( int argc, char *argv[] )
 {
